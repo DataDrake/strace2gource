95890ad6e43c8564d7db91ff15d064f0df0c5ec0
Bryan T. Meyers <bmeyers@datadrake.com>
2016-08-20 20:40:09 -0400

    workspace cleanup

--- strace2gource.go
+++ strace2gource.go
@@ -12,6 +12,7 @@ import (
 
 var r *regexp.Regexp
 var t0 int64 = -1
+
 func ParseLine(line string) {
 	match := r.FindStringSubmatch(line)
 
@@ -21,17 +22,17 @@ func ParseLine(line string) {
 		filename := ""
 		found := true
 		switch match[3] {
-		case "open","openat","mkdir","mkdirat":
+		case "open", "openat", "mkdir", "mkdirat":
 			operation = 'A'
-			filename = strings.Split(match[4],"\"")[1]
-		case "read","lseek","fstat","getdents":
+			filename = strings.Split(match[4], "\"")[1]
+		case "read", "lseek", "fstat", "getdents":
 			operation = 'A'
-			if strings.Contains(match[4],"<") && strings.Contains(match[4],">"){
+			if strings.Contains(match[4], "<") && strings.Contains(match[4], ">") {
 				filename = strings.Split(match[4], "<")[1]
 				filename = strings.Split(filename, ">")[0]
 			}
-		case "close","fcntl","write":
-			if strings.Contains(match[4],"<") && strings.Contains(match[4],">"){
+		case "close", "fcntl", "write":
+			if strings.Contains(match[4], "<") && strings.Contains(match[4], ">") {
 				filename = strings.Split(match[4], "<")[1]
 				filename = strings.Split(filename, ">")[0]
 			}
@@ -40,12 +41,12 @@ func ParseLine(line string) {
 		}
 
 		if found {
-			timestamp := strings.Join([]string{"Jan 12",match[2]}," ")
-			unix,_ := time.Parse(time.StampMicro,timestamp)
+			timestamp := strings.Join([]string{"Jan 12", match[2]}, " ")
+			unix, _ := time.Parse(time.StampMicro, timestamp)
 			if t0 == -1 {
 				t0 = unix.UnixNano()
 			}
-			fmt.Printf("%d|%s|%c|%s\n", (unix.UnixNano() - t0) / 1000, process, operation, filename)
+			fmt.Printf("%d|%s|%c|%s\n", (unix.UnixNano()-t0)/1000, process, operation, filename)
 		}
 	}
 }
@@ -58,20 +59,18 @@ func ReadGzFile(fi *os.File) error {
 	}
 	defer fz.Close()
 
-	buf := bufio.NewReaderSize(fz,10000)
+	buf := bufio.NewReaderSize(fz, 10000)
 
-	s,err := buf.ReadString('\n')
+	s, err := buf.ReadString('\n')
 	for err == nil {
 		ParseLine(s)
-		s,err = buf.ReadString('\n')
+		s, err = buf.ReadString('\n')
 	}
 	return err
 }
 
-
-
 func main() {
-	r, _= regexp.Compile("^(\\d+) (\\S+.\\d+) (\\w+)\\((.*)\\)")
+	r, _ = regexp.Compile("^(\\d+) (\\S+.\\d+) (\\w+)\\((.*)\\)")
 
 	if len(os.Args) > 1 {
 		fi, err := os.Open(os.Args[1])
@@ -84,4 +83,3 @@ func main() {
 		ReadGzFile(os.Stdin)
 	}
 }
-
